1b6c9979ee3f968ea9eb72f3529d6d8448c05da6
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-11 22:42:11 +0800

    email js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.png";
-// import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import ContactUs from "./Contact";
 
 function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        {/* <Particle /> */}
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
