496bf202a0db5e877b921232934408aee3466f1c
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-11 23:13:02 +0800

    update form

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,13 +1,18 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
-export const ContactUs = () => {
+export function ContactUs() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+    emailjs.sendForm(
+        process.env.REACT_APP_EmailJS_SERVICE_ID,
+        process.env.REACT_APP_EmailJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EmailJS_PUBLIC_KEY
+    )
       .then((result) => {
           console.log(result.text);
       }, (error) => {
@@ -18,9 +23,9 @@ export const ContactUs = () => {
   return (
     <form ref={form} onSubmit={sendEmail}>
       <label>Name</label>
-      <input type="text" name="user_name" />
+      <input type="text" name="from_name" />
       <label>Email</label>
-      <input type="email" name="user_email" />
+      <input type="email" name="from_email" />
       <label>Message</label>
       <textarea name="message" />
       <input type="submit" value="Send" />
