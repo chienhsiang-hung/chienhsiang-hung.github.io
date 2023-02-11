0eea1813262c44290fdc6e887a5e0da0056c2618
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-11 11:46:38 +0800

    home.jpg

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,7 +9,7 @@ function Home() {
   return (
     <section>
       <Container fluid className="home-section" id="home">
-        <Particle />
+        {/* <Particle /> */}
         <Container className="home-content">
           <Row>
             <Col md={7} className="home-header">
