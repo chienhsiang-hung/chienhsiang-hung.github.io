360422bdeff3cffd535c9d71574928bba66fcd4c
chienhsiang-hung <77676044+chienhsiang-hung@users.noreply.github.com>
2023-02-12 13:45:24 +0800

    adjust contact

--- src/components/Home/Contact.js
+++ src/components/Home/Contact.js
@@ -1,7 +1,6 @@
 import React, { useRef } from 'react';
 import emailjs, { send } from '@emailjs/browser';
 import "./Contact.css"
-import { Col } from 'react-bootstrap';
 
 export function ContactUs() {
   const form = useRef();
@@ -23,45 +22,33 @@ export function ContactUs() {
   };
 
   return (
-    <Col md={12} className="home-about-social form-parent">
-      <> 
-        <form ref={form} onSubmit={sendEmail}>
-          <div class="form">
-            <div class="title">Opportunities</div>
-            <div class="subtitle">Requests or Questions?<br/>Don't hesitate to contact me.</div>
-            <div class="input-container ic1">
-              <input name='from_name' id="from_name" class="input" type="text" placeholder=" " />
-              <div class="cut"></div>
-              <label for="from_name" class="placeholder">Name</label>
-            </div>
-
-            <div class="input-container ic2">
-              <input name='from_email' id="from_email" class="input" type="text" placeholder=" " />
-              <div class="cut"></div>
-              <label for="from_email" class="placeholder">Email</label>
-            </div>
-
-
-            <div class="input-container ic2" style={{height:"100px"}}>
-              <textarea name="message" id="message" class="input" type="text" placeholder=" " />
-              <div class="cut"></div>
-              <label for="message" class="placeholder">Message</label>
-            </div>
-            
-            <button type="text" class="submit" value="Send">Submit</button>
+    <> 
+      <form ref={form} onSubmit={sendEmail}>
+        <div class="form">
+          <div class="title">Opportunities</div>
+          <div class="subtitle">Requests or Questions?<br/>Don't hesitate to contact me.</div>
+          <div class="input-container ic1">
+            <input name='from_name' id="from_name" class="input" type="text" placeholder=" " />
+            <div class="cut"></div>
+            <label for="from_name" class="placeholder">Name</label>
           </div>
-        </form>
 
-        {/* <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="from_name" />
-          <label>Email</label>
-          <input type="email" name="from_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <input type="submit" value="Send" />
-        </form> */}
-      </>
-    </Col>
+          <div class="input-container ic2">
+            <input name='from_email' id="from_email" class="input" type="text" placeholder=" " />
+            <div class="cut"></div>
+            <label for="from_email" class="placeholder">Email</label>
+          </div>
+
+
+          <div class="input-container ic2" style={{height:"100px"}}>
+            <textarea name="message" id="message" class="input" type="text" placeholder=" " />
+            <div class="cut"></div>
+            <label for="message" class="placeholder">Message</label>
+          </div>
+          
+          <button type="text" class="submit" value="Send">Submit</button>
+        </div>
+      </form>
+    </>
   );
 };
